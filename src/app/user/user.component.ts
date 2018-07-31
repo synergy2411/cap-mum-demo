60840d7650900dc4272e5fad3f59d05e87cc01c6
synergy2411 <synergy2411@outlook.com>
2018-07-31 11:40:41 +0530

    two way data binding added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,6 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({    
     selector : 'app-user',
-    template : `<h2>User Component Loaded!!</h2>`
+    // template : `<h2>User Component Loaded!!</h2>`
+    templateUrl : './user.component.html'
 })
-export class UserComponent{}
+export class UserComponent{
+
+    moreInfo(user){
+        alert(`${user.firstName} is working with ${user.company}!!!` );
+    }
+
+    user = {
+        firstName : "Bill",
+        lastName : "Gates",
+        dob : new Date ("Dec 21, 1965"),
+        income : 50000,
+        isWorking : true,
+        company : "Microsoft",
+        image : "assets/images/users_images/bill.jpg",
+        vote : 120
+    }
+}
